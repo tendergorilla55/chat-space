4f5f30a17d98f5b0eb5797134b2a8eb49fb201a3
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2018-12-12 21:04:00 +0900

    Edit group_create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resources :messages
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
