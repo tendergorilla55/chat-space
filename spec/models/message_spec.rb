4bdc23da9da4cbfb7f28f4c89e938a23071f9904
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2018-12-18 20:49:50 +0900

    Edit message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,27 +1,39 @@
 require 'rails_helper'
 
-describe Message do
+RSpec.describe Message, type: :model do
   describe '#create' do
+    context 'can save' do
+      it "is valid with body" do
+        expect(build(:message, image:nil)).to be_valid
+      end
 
-    it "is valid with body" do
-      message = build(:message, image: nil)
-      expect(message).to be_valid
+      it "is valid with image" do
+        expect(build(:message, body: nil)).to be_valid
+      end
+
+      it "is valid with body, image" do
+        expect(build(:message)).to be_valid
+      end
     end
 
-    it "is valid with image" do
-      message = build(:message, body: nil)
-      expect(message).to be_valid
+    context 'can not save' do
+      it "is invalid without body, image" do
+        message = build(:message, body: nil, image: nil)
+        message.valid?
+        expect(message.errors[:body]).to include('を入力してください')
+      end
+
+      it "is invalid without group_id" do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to include('を入力してください')
+      end
+
+      it "is invalid without body, image" do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user]).to include('を入力してください')
+      end
     end
-
-    it "is valid with body, image" do
-      message = build(:message)
-      expect(message).to be_valid
-    end
-
-    it "is invalid without body, image" do
-      message = build(:message, body: nil, image: nil)
-      message.valid?
-      expect(message.errors)
-
   end
 end
