6a12a42a30d8e6839bab0151e6469db885b883f6
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2019-01-05 16:09:18 +0900

    Edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
-  function buildHTML(data){
-    var image = message.image ? `<img src="${message.image}"> ` : ""
+  function buildHTML(message){
+    var image = message.image ? `<img src="${message.image}" class="chat-main__message-body"> ` : "" ;
     var html = `<div class="chat-main__message clearfix">
                   <div class="chat-main__message-name">
                     ${message.name}
@@ -11,31 +11,28 @@ $(function(){
                   <p class="chat-main__message-body">
                     ${message.body}
                   </p>
-                  <p class="chat-main__message-body">
-                    ${image}
-                  </p>
+                  ${image}
                 </div>`
     return html;
   }
   $('#send_message').on('submit', function(e){
     e.preventDefault();
-    console.log("送信");
     var formData = new FormData(this);
-    console.log(this);
     var url = $(this).attr('action')
     $.ajax({
       url: url,
-      type: "POST",
+      type: 'POST',
       data: formData,
-      datatype: 'json',
+      dataType: 'json',
       processData: false,
       contentType: false
     })
     .done(function(data){
+      console.log(data);
       var html = buildHTML(data);
       $('.chat-main__body--messages-list').append(html)
       $('.message').val('')
-      $('.chat-main__body--messages-list').animate({scrollTop: $('.chat-main__body--messages-list')[0].scrollHeight}, 'fast')
+      $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast')
     })
     .fail(function(){
       alert('error');
