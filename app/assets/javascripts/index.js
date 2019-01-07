34354bac4019b1520dc74e0e531e4750df59b44f
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2019-01-07 20:37:32 +0900

    Edit js file

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,21 +1,21 @@
 $(function() {
-  var search_list = $('#user-search-result');
+  var searchList = $('#user-search-result');
   function searchUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
-    search_list.append(html);
+    searchList.append(html);
   };
 
-  var member_list = $('#chat-group-users');
+  var memberList = $('#chat-group-users');
   function addUser(id, name) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${id}'>
                   <input name='group[user_ids][]' type='hidden' value='${id}'>
                   <p class='chat-group-user__name'>${name}</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
                 </div>`
-    member_list.append(html);
+    memberList.append(html);
   };
 
   $('#user-search-field').on('keyup', function() {
@@ -29,7 +29,7 @@ $(function() {
     })
 
     .done(function(users) {
-      search_list.empty();
+      searchList.empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
           searchUser(user);
@@ -42,9 +42,9 @@ $(function() {
   });
 
   $(document).on('click', '.user-search-add', function() {
-    var user_id = $(this).data('user-id');
-    var user_name = $(this).data('user-name');
-    addUser(user_id, user_name);
+    var userId = $(this).data('user-id');
+    var userName = $(this).data('user-name');
+    addUser(userId, userName);
     $(this).parent().remove();
   });
 
