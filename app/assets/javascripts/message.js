9e9d9f558ce5dd893602f324431d322a264faf9e
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2019-01-07 22:46:02 +0900

    Create code for automatic update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(message){
     var image = message.image ? `<img src="${message.image}" class="chat-main__message-body"> ` : "" ;
-    var html = `<div class="chat-main__message clearfix">
+    var html = `<div class="chat-main__message clearfix" deta-message-id="${message.id}">
                   <div class="chat-main__message-name">
                     ${message.name}
                   </div>
@@ -17,12 +17,12 @@ $(function(){
   }
 
   function clearBox(html){
-    $('.chat-main__body--messages-list').append(html)
-    $('.message').val('')
-    $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast')
+    $('.chat-main__body--messages-list').append(html);
+    $('.message').val('');
+    $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
   }
 
-  $('#send_message').on('submit', function(e){
+  $('#send_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -34,12 +34,36 @@ $(function(){
       processData: false,
       contentType: false
     })
-    .done(function(data){
+    .done(function(data) {
       var html = buildHTML(data);
       clearBox(html);
     })
-    .fail(function(){
+    .fail(function() {
       alert('error');
     })
-  })
+  });
+
+  var interval = setInterval(function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({
+        url: location.href.json
+      })
+      .done(function(messages) {
+        var id = $('.chat-main__message').data('messageId');
+        var insertHTML = "";
+        messages.forEach(function(message) {
+          if (message.id > id) {
+            insertHTML += buildHTML(message);
+          }
+        });
+        $('.chat-main__body--messages-list').append(insertHTML);
+        $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert("自動更新に失敗しました");
+      })
+    } else {
+      clearInterval(interval);
+    }
+  }, 5000);
 });
---
+++ app/views/messages/index.json.jbuilder
@@ -0,0 +1,7 @@
+json.array! @messages do |message|
+  json.name message.user.name
+  json.time message.created_at
+  json.body message.body
+  json.image message.image
+  json.id message.id
+end
